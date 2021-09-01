cf8e29622ee95b9362dd203b3bfc01e3e36eb6de
Sohail Ahmed <sohail.comsians@gmail.com>
2021-09-01 17:35:56 +0500

    converted to class

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ import time
 from selenium.webdriver.common.by import By
 
 
-options = Options()
-# options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument(
-    "user-agent=[Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36]")
+class GmailNameChecker:
 
-driver = webdriver.Chrome('./driver/chromedriver', chrome_options=options)
-url = 'https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp'
-driver.get(url)
-for x in range(1000):
-    input1 = driver.find_element_by_id('username')
-    input1.send_keys("sohail"+str(x))
-    input2 = input1.send_keys(Keys.TAB)
-    time.sleep(2)
-    error = driver.find_element_by_class_name("LXRPh").text
-    print(error)
-    time.sleep(2)
-    input1.clear()
-    driver.quit()
+    def __init__(self):
+        options = Options()
+        # options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36")
+        driver = webdriver.Chrome(
+            './driver/chromedriver', chrome_options=self.options)
+        url = 'https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp'
+        browser = driver.get(url)
+
+    def check_username(self):
+        for x in range(1000):
+            input1 = self.browser.find_element_by_id('username')
+            input1.send_keys("you name here"+str(x))
+            input2 = input1.send_keys(Keys.TAB)
+            time.sleep(2)
+            error = self.browser.find_element_by_class_name("LXRPh").text
+            print(error)
+            time.sleep(2)
+            input1.clear()
+            self.browser.quit()
